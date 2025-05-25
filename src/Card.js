d113556f0105d18168b386568c2d40d9a9de55c0
saurav022 <sauravkmr022@gmail.com>
2025-05-25 16:01:39 +0530

    patna

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = () => {
   return (
     <div className="cardIC">
       <div className="containerIC">
-        <h4 className="h4class"><b>UPCOMING EVENT</b></h4>
+        <h4 className="h4class"><b>UPCOMING EVENT In Patna</b></h4>
         <p className="pclass">Patna, June 1st!</p>
          <a href="https://forms.gle/crm464Lt4PYA33sr6" className="btn" target="_blank" rel="noopener noreferrer">
   Register Now
